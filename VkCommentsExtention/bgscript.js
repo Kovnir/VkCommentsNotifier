844d815d4cbb6c3a34290ff9a687df7a0d9fb2ec
Albert <akovnir@playq.net>
2018-06-20 23:55:25 +0300

    First commit

---
+++ VkCommentsExtention/bgscript.js
@@ -0,0 +1,241 @@
+chrome.tabs.onUpdated.addListener(ListenerHandler);
+
+Loop();
+
+function ListenerHandler(tabId, changeInfo, tab) 
+{
+	if (changeInfo.status == "complete")
+	{
+		if (tab.url.includes('oauth.vk.com') && tab.url.includes('access_token'))
+		{
+			chrome.storage.local.get('access_token', function (result)
+			{
+				if (result.access_token === undefined)
+				{
+					chrome.tabs.remove(tabId);
+					GetAccessToken(tab.url);
+				}
+			});
+		}
+	}
+}
+
+function GetAccessToken(url)
+{
+	var accessToken = GetUrlParameterValue(url, 'access_token');
+	if (accessToken === undefined || accessToken.length === undefined) 
+	{
+		alert('Не удалось получить токен');
+		return;
+	}
+
+	var userId = GetUrlParameterValue(url, 'user_id');
+	if (userId === undefined || userId.length === undefined) 
+	{
+		alert('Не удалось получить userId');
+		return;
+	}
+
+	chrome.storage.local.set({'access_token': accessToken, "userId" : userId}, function ()
+	{
+		chrome.tabs.create({url:"group_loading_page.html"}, function(tab)
+		{
+			LoadGroups(tab.id, userId, accessToken);
+		});
+	});
+}
+
+function LoadGroups(tabId, userId, accessToken)
+{
+	var xhr = new XMLHttpRequest();
+
+   	url = "https://api.vk.com/method/groups.get?user_id=" + userId + "&filter=admin,editor,moder&extended=1&access_token=" + accessToken + "&v=5.78";
+
+	xhr.open('GET', url, true);
+
+	xhr.send();
+
+	xhr.onreadystatechange = function() 
+	{
+		if (xhr.readyState != 4) 
+		{
+			return;
+		}
+		if (xhr.status != 200) 
+		{
+			alert("Не удалось получить список гупп.\nСтатус: " + xhr.status + '\nТекст: ' + xhr.statusText + "\nПроверьте интерен соединение и доступ к сайту Вконтакте и попробуйте еще раз.");
+		} 
+		else
+		{
+			chrome.storage.local.set({'group_list': xhr.responseText}, function ()
+			{
+				chrome.tabs.remove(tabId);
+				chrome.tabs.create({url:"page_after_group_loading.html"});
+			});
+		}
+	}
+}
+
+function GetUrlParameterValue(url, parameterName) {
+	"use strict";
+
+	var urlParameters  = url.substr(url.indexOf("#") + 1),
+		parameterValue = "",
+		index,
+		temp;
+
+	urlParameters = urlParameters.split("&");
+
+	for (index = 0; index < urlParameters.length; index += 1) {
+		temp = urlParameters[index].split("=");
+
+		if (temp[0] === parameterName) {
+			return temp[1];
+		}
+	}
+
+	return parameterValue;
+}
+
+
+function Scedule(timeout)
+{
+	setTimeout(Loop, timeout);
+}
+
+function Loop()
+{
+	chrome.storage.local.get(['access_token', 'group_id', 'comments'], function(result) 
+	{
+		if (result.access_token === undefined || result.group_id === undefined)
+		{
+			Scedule(1000);
+			return;
+		}
+		oldData = result.comments;
+		GetWallDataWithOffset(1, 0, oldData, undefined, undefined, result.group_id, result.access_token,
+			function(newComments, newData, totalComments, totalPosts)
+			{
+				chrome.storage.local.get(['new_comments'], function(result) 
+				{
+					var oldNewComments = result.new_comments;
+					if (oldNewComments === undefined)
+					{
+						oldNewComments = {};
+					}
+					else
+					{
+						for(var key in newComments)
+						{
+							var item = newComments[key];
+							alert("Новые комментарии в Вашей группе!\n"+
+								"Количество: "+ item.count + "\nПост: " + item.text);
+							if (oldNewComments[item.id] === undefined)
+							{
+								oldNewComments[item.id] = {};
+								oldNewComments[item.id].id = item.id;
+								oldNewComments[item.id].count = 0;
+							}
+							oldNewComments[item.id].text = item.text;
+							oldNewComments[item.id].count += item.count;
+						}
+					}
+					chrome.storage.local.set({'new_comments': oldNewComments, 
+						'comments' : newData, 
+						'total_comments' : totalComments,
+						'total_posts': totalPosts}, function ()
+					{
+						Scedule(30000);
+					});
+				});
+			});
+	});
+}
+
+function GetWallDataWithOffset(offset, totalComments, oldData, newData, newComments, groupId, accessToken, callback)
+{
+	if (newData === undefined)
+	{
+		newData = {};
+	}
+	if (newComments === undefined)
+	{
+		newComments = {};
+	}
+
+	var xhr = new XMLHttpRequest();
+	url = "https://api.vk.com/method/wall.get?domain=" + groupId + "&count=100&offset=" + offset + "&extended=0&access_token=" + accessToken + "&v=5.78";
+	xhr.open('GET', url, true);
+	xhr.send();
+
+	xhr.onreadystatechange = function() 
+	{
+		if (xhr.readyState != 4) 
+		{
+			return;
+		}
+		if (xhr.status != 200) 
+		{
+//			alert(xhr.status + ': ' + xhr.statusText);
+			Scedule(30000);
+			return;
+		}
+		else
+		{
+			try 
+			{
+				var response = JSON.parse(xhr.responseText)["response"];
+				var posts = response && response["items"];
+				var postsCount = response && response["count"];
+
+				if (response === undefined || posts === undefined || postsCount === undefined)
+				{
+					Scedule();
+					return;
+				}
+				posts.forEach(function(item, i, arr) 
+				{
+					var count = item.comments.count;
+					if (count > 0)
+					{
+						var oldCount = 0
+						if (oldData != undefined && oldData[item.id] != undefined)
+						{
+							oldCount = oldData[item.id];
+						}
+						if (oldCount < count)
+						{
+							newComments[item.id] = {};
+							newComments[item.id].id = item.id;
+							var text = item.text;
+							if (text.length > 100)
+							{
+								text = text.substr(0,100) + "...";
+							}
+							newComments[item.id].text = text;
+							newComments[item.id].count = count - oldCount;
+						}
+						newData[item.id] = count;
+						totalComments += count;
+					}
+				});
+				var restPosts = postsCount - (posts.length + offset - 1);
+				console.log('postsCount: ' + postsCount);
+
+				if (restPosts > 0)
+				{
+					GetWallDataWithOffset(offset + 100, totalComments, oldData, newData, newComments, groupId, accessToken, callback);
+				}
+				else
+				{
+					callback(newComments, newData, totalComments, postsCount);
+				}
+			} 
+			catch(e)
+			{
+				alert('Ошибка ' + e.name + ":" + e.message + "\n" + e.stack);
+				Scedule(30000);
+			}
+		}
+	}
+}
---
+++ VkCommentsExtention/script.js
@@ -0,0 +1,227 @@
+document.addEventListener('DOMContentLoaded', Setup);
+
+
+//variables
+var groups;
+
+function Setup()
+{
+	Subscribe();
+	OpenSuitibleScreen();
+}
+
+function OpenSuitibleScreen()
+{
+	chrome.storage.local.get(['access_token', 'group_list', 'group_id', 'group_name', "group_numeric_id", 'total_comments', 'total_posts', 'new_comments'], function(result) 
+	{
+		document.getElementById("cleat_all").style.display = 'none';
+		document.getElementById("clear_all_button").style.display = 'block';
+		if (result.access_token === undefined)
+		{
+			ShowLogin();
+		}
+		else
+		{
+			if (result.group_list === undefined)
+			{
+				ShowGroupsLoading();
+			}
+			else
+			{
+				if (result.group_id === undefined)
+				{
+					ShowSetup(result.group_list);
+				}
+				else
+				{
+					ShowComments(result.group_id, result.group_name, result.group_numeric_id, result.total_posts, result.total_comments, result.new_comments);
+				}
+			}
+		}
+	});
+}
+
+		
+function OnLogicButtonClick() 
+{
+	var vkCLientId = '6602088',
+	vkAuthenticationUrl = 'https://oauth.vk.com/authorize?client_id=' + vkCLientId + '&scope=offline&redirect_uri=http%3A%2F%2Foauth.vk.com%2Fblank.html&display=page&response_type=token';
+	console.log('vkAuthenticationUrl is ' + vkAuthenticationUrl);
+	chrome.tabs.create({url: vkAuthenticationUrl, active: true});
+}
+
+function OnGroupSelectButtonClick()
+{
+	var selectedGroup = groups[document.getElementById("group_select").selectedIndex];
+	var screenName = selectedGroup.screen_name;
+	var name = selectedGroup.name;
+	var groupNumericId = selectedGroup.id;
+	chrome.storage.local.set({'group_id': screenName, 'group_name': name, 'group_numeric_id' : groupNumericId}, function ()
+	{
+		OpenSuitibleScreen();   
+	});
+}
+
+function OnClearButtonClick() 
+{
+	document.getElementById("cleat_all").style.display = 'block';
+	document.getElementById("clear_all_button").style.display = 'none';
+	document.getElementById("login").style.display = 'none';
+	document.getElementById("group_loading").style.display = 'none';
+	document.getElementById("setup").style.display = 'none';
+	document.getElementById("comments").style.display = 'none';
+}
+
+function Subscribe()
+{
+	var button = document.getElementById("login_button");
+	button.addEventListener("click", OnLogicButtonClick);
+
+	button = document.getElementById("group_select_button");
+	button.addEventListener("click", OnGroupSelectButtonClick);
+
+	button = document.getElementById("clear_all_button");
+	button.addEventListener("click", OnClearButtonClick);
+
+	var no = document.getElementById("no_clear_button");
+	var yes = document.getElementById("yes_clear_button");
+
+	no.addEventListener("click", function() 
+	{
+		OpenSuitibleScreen();
+	});
+	yes.addEventListener("click", function() 
+	{
+		groups = undefined;
+
+		chrome.storage.local.clear(function() 
+		{
+			var error = chrome.runtime.lastError;
+			if (error) {
+				alert("Чистка прошла с ошибкой!");
+			}
+			else
+			{
+				alert("Чистка прошла успешно!");
+			}
+			OpenSuitibleScreen();
+		});
+	});
+
+}
+
+function ShowGroupsLoading()
+{
+	document.getElementById("login").style.display = 'none';
+	document.getElementById("group_loading").style.display = 'block';
+	document.getElementById("setup").style.display = 'none';
+	document.getElementById("comments").style.display = 'none';
+}
+
+function ShowSetup(text)
+{
+	document.getElementById("login").style.display = 'none';
+	document.getElementById("group_loading").style.display = 'none';
+	document.getElementById("setup").style.display = 'block';
+	document.getElementById("comments").style.display = 'none';
+
+	var select = document.getElementById("group_select");
+
+	var json = JSON.parse(text);
+
+	groups = json["response"]["items"];
+	groups.forEach(function(item, i, arr) 
+	{
+		var newOption = new Option(item.name, item.name, true, true)
+		select.appendChild(newOption);
+	});
+}
+
+function ShowLogin()
+{
+	document.getElementById("clear_all_button").style.display = 'none';
+	document.getElementById("login").style.display = 'block';
+	document.getElementById("group_loading").style.display = 'none';
+	document.getElementById("setup").style.display = 'none';
+	document.getElementById("comments").style.display = 'none';
+}
+
+function ShowComments(groupId, groupName, groupNumericId, postCount, commentsCount, newComments)
+{
+	document.getElementById("login").style.display = 'none';
+	document.getElementById("group_loading").style.display = 'none';
+	document.getElementById("setup").style.display = 'none';
+	document.getElementById("comments").style.display = 'block';
+
+	var nameDiv = document.getElementById('group_name');
+	var text = "Трекаем группу: <a href=\"http://vk.com/" + groupId + "\" target=\"_blank\">" + groupName + "</a>";
+	nameDiv.innerHTML = text;
+
+	var gettingsData = document.getElementById('getting_data');
+	var postCountDiv = document.getElementById('total_posts');
+	var countComments = document.getElementById('total_comments');
+	var noNewComments = document.getElementById('no_new_comments');
+	var commentsHolder = document.getElementById('new_comments');
+
+	if (postCount === undefined)
+	{
+		gettingsData.style.display = 'block';
+		postCountDiv.style.display = 'none';
+		countComments.style.display = 'none';
+		noNewComments.style.display = 'none';
+		setTimeout(OpenSuitibleScreen, 1000);
+		return;
+	}
+	gettingsData.style.display = 'none';
+	postCountDiv.style.display = 'block';
+	countComments.style.display = 'block';
+
+	text = "Всего постов: " + postCount;
+	postCountDiv.innerHTML = text;
+
+	text = "Всего комментариев: " + commentsCount;
+	countComments.innerHTML = text;
+
+	if (newComments.length == 0)
+	{
+		commentsHolder.style.display = 'none';
+		noNewComments.style.display = 'block';
+	}
+	else
+	{
+		noNewComments.style.display = 'none';
+		commentsHolder.style.display = 'block';
+		commentsHolder.innerHTML = "";
+
+		for(var key in newComments)
+		{
+			let item = newComments[key];
+			let commentText = "<div class=\"comment\">" +
+				"<p>Новых комментариев: " + item.count + "</p>" + 
+				"<p>К посту:</p>" +
+				"<div class=\"post_text\">" + item.text + "</div>" +
+				"<p><button id=\"open_button" + item.id + "\">Открыть!</button><button id=\"checked_button" + item.id + "\" class=\"button button_blue\">Проверено!</button></p>" +
+				"</div>";
+
+			commentsHolder.innerHTML += commentText;
+		}
+		for(let key in newComments)
+		{
+			let item = newComments[key];
+			let button_open = document.getElementById("open_button" + item.id);
+			button_open.addEventListener("click", function() 
+			{
+				chrome.tabs.create({url: "http://vk.com/" + groupId + "?w=wall-" + groupNumericId + "_" + item.id, active: true});
+			});
+			let button_checked = document.getElementById("checked_button" + item.id);
+			button_checked.addEventListener("click", function() 
+			{
+				delete newComments[key];
+				chrome.storage.local.set({'new_comments': newComments}, function ()
+					{
+						OpenSuitibleScreen();
+					});
+			});
+		}
+	}
+}
